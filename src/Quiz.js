bd8c7e85e0da9f32d452b99e81e1c274a539d56f
Burns, Patrick <pburns@allegion.com>
2020-05-15 14:17:01 -0400

    updated text

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
     }
     render() {
         return <div>
-            <div className="QuizQuestion">{{ quizData.quizQuestions[0].instruction_text }}</div>
+            <div className="QuizQuestion">quizData.quizQuestions[0].instruction_text</div>
         </div>
     }
 }
